69e43249de929e0fb1db68bba3c4c155c23e7eba
root <root@localhost.localdomain>
2026-03-24 21:06:19 +0800

    feat: 添加 3月24日创作内容
    - 新增诗歌《春夜微凉》
    - 新增赛博朋克春夜配图
    - 更新 Gallery 组件

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -13,6 +13,15 @@ interface Photo {
 }
 
 const photos: Photo[] = [
+  // 3月24日 - 春夜微凉
+  {
+    id: '24a',
+    title: '春夜微凉',
+    location: '创作',
+    date: '2026-03-24',
+    description: '霓虹落地成花瓣，风里混着咖啡香',
+    image: '/gallery/2026-03-24-spring-night-reflection/spring-night-neon.jpg'
+  },
   // 3月23日 - 赛博春夜
   {
     id: '23a',
